ee624e94cb9c69650f222217e99ac53bb63ed6ad
Leonardo Melati <leonardomelati85@gmail.com>
2023-10-30 08:41:53 -0300

    fix: adjust type error on build library

--- rollup.config.js
+++ rollup.config.js
@@ -1,42 +1,46 @@
-import esbuild, { minify } from "rollup-plugin-esbuild";
-import dts from "rollup-plugin-dts";
-import size from "rollup-plugin-size";
+import esbuild, { minify } from 'rollup-plugin-esbuild'
+import dts from 'rollup-plugin-dts'
+import size from 'rollup-plugin-size'
 
 export default [
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: {
       file: `build/lib/index.js`,
-      format: "cjs",
+      format: 'cjs',
       sourcemap: true,
     },
     plugins: [esbuild(), size(), minify()],
+    external: ['rxjs'],
   },
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: {
       file: `build/lib/index.esm.js`,
-      format: "es",
+      format: 'es',
       sourcemap: true,
     },
     plugins: [esbuild(), size(), minify()],
+    external: ['rxjs'],
   },
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: {
       file: `build/lib/index.mjs`,
-      format: "es",
+      format: 'es',
       sourcemap: true,
     },
     plugins: [esbuild(), size(), minify()],
+    external: ['rxjs'],
   },
   {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: {
       file: `build/lib/index.d.ts`,
-      format: "es",
+      format: 'es',
       sourcemap: true,
     },
     plugins: [esbuild(), dts(), size()],
+    external: ['rxjs'],
   },
-];
+]
